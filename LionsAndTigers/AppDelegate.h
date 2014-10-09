74f84fb3a8eec959cea8ae1f4ae00dc31aa83b22
Wade Sellers <Waderoni@gmail.com>
2014-10-09 15:55:31 -0500

    Initial Commit

---
+++ LionsAndTigers/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LionsAndTigers
+//
+//  Created by Wade Sellers on 10/9/14.
+//  Copyright (c) 2014 Wade Sellers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
